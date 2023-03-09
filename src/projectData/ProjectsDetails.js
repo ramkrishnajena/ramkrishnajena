c8a5b0e717e6671c1d84f6047c622e959a92705c
ramkrishnajena <contact@ramkrishnajena.com>
2023-03-09 18:28:15 +0530

    i have not connected any backend so i have created a projects file where all details available

---
+++ src/projectData/ProjectsDetails.js
@@ -0,0 +1,32 @@
+export let projectData = [
+  {
+    id: 1,
+    imageUrl:
+      "https://github.com/ramkrishnajena/ramkrishnajena/blob/main/src/asset/Banklet.jpg?raw=true",
+    altTag: "Banklet",
+    tech: "HTML, CSS, JS",
+    pageUrl: "https://github.com/ramkrishnajena/banklet",
+    about: "Banklet - Banking app for user",
+    visit: "",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://github.com/ramkrishnajena/ramkrishnajena/blob/main/src/asset/ytdownload.jpg?raw=true",
+    altTag: "YT Downloader",
+    tech: "HTML, CSS, JS",
+    pageUrl: "https://github.com/ramkrishnajena/yt-downloader",
+    about: "YT Downloader",
+    visit: "",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://github.com/ramkrishnajena/ramkrishnajena/blob/main/src/asset/ramkrishnajena.JPG?raw=true",
+    altTag: "Portfolio",
+    tech: "REACT JS",
+    pageUrl: "https://github.com/ramkrishnajena/ramkrishnajena",
+    about: "Ramkrishna Jena - Portfolio Websites",
+    visit: "",
+  },
+];
