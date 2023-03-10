683201605ce8012f30e2a43201ba374c1fe0c0d2
ramkrishnajena <contact@ramkrishnajena.com>
2023-03-10 23:14:36 +0530

    add the social links and make some minor css changes for them

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -12,16 +12,22 @@ const AboutMe = () => {
             <img src={profilePhoto} alt="myphoto" />
           </div>
           <div className="about__photo--social__icons">
-            <FacebookOutlined />
-            <LinkedIn />
-            <GitHub />
+            <a href="https://github.com/ramkrishnajena">
+              <GitHub />
+            </a>
+            <a href="https://www.linkedin.com/in/ramkrishnajena/">
+              <LinkedIn />
+            </a>
+            <a href="https://www.facebook.com/akaramkrishnajena/">
+              <FacebookOutlined />
+            </a>
           </div>
         </div>
         <div className="about__desc">
           <h4>About Me</h4>
           <p className="about__desc--para">
             I am an experienced front-end web developer with extensive knowledge
-            of HTML, CSS, and JavaScript, as well as proficiency in Bootstrap5,
+            of HTML, CSS, and JavaScript, as well as proficiency in Bootstrap 5,
             Tailwind CSS, and ReactJS. I possess a strong sense of design
             aesthetics and a deep understanding of user experience, enabling me
             to create visually captivating and responsive websites that keep
